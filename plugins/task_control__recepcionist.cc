46ded49b7b8f8136283d1ffa73450f6431a39119
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-03-27 14:29:22 -0300

    Add recepcionist task control

--- plugins/task_control__recepcionist.cc
+++ plugins/task_control__recepcionist.cc
@@ -1,53 +1,72 @@
-#include <gazebo/gazebo.hh>
-#include <gazebo/transport/transport.hh>
-#include <gazebo/msgs/msgs.hh>
-
-#include <ignition/math/Vector2.hh>
-
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/physics/World.hh>
 
 #include <iostream>
 
+#include <ros/ros.h>
+#include <std_srvs/Empty.h>
+#include <std_msgs/String.h>
+
 namespace gazebo
 {
-
-  class TaskControl_Recepcionist : public WorldPlugin
+  class GAZEBO_VISIBLE TaskControl_Recepcionist : public WorldPlugin
   {
 
-    public: TaskControl_Recepcionist() : WorldPlugin()
+    // ros node
+    private: std::unique_ptr<ros::NodeHandle> rosNode{nullptr};
+
+    // services
+    private: ros::ServiceServer srv_start;
+
+    // Publishers
+    public: ros::Publisher pub_door_1;
+
+    // world
+    private: physics::WorldPtr world;
+
+    public: TaskControl_Recepcionist()
     {
-      std::cout << "Start TaskControl_Recepcionist!" << std::endl;
-
-      // Create our node for communication
-      gazebo::transport::NodePtr node(new gazebo::transport::Node());
-      node->Init();
-
-      // Publish to a Gazebo topic
-      gazebo::transport::PublisherPtr pub =
-        node->Advertise<gazebo::msgs::Vector2d>("~/pose_example1");
-
-      // Wait for a subscriber to connect
-      pub->WaitForConnection();
-
-      // // Publisher loop...replace with your own code.
-      // while (true)
-      // {
-      //   gazebo::common::Time::MSleep(100);
-      //   ignition::math::Vector2<double> vect(5, 7);
-      //   gazebo::msgs::Vector2d msg;
-      //   gazebo::msgs::Set(&msg, vect);
-      //   pub->Publish(msg);
-      // }
-      //
-      // // Make sure to shut everything down.
-      // gazebo::transport::fini();
     }
 
-    public: void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
+    public: void Load(gazebo::physics::WorldPtr _world, sdf::ElementPtr _sdf)
     {
+      std::cout << "Starting task simulation: Recepcionist!" << std::endl;
 
+      this->world = _world;
+
+      if (!ros::isInitialized())
+      {
+        int argc = 0;
+        char **argv = NULL;
+        ros::init(argc, argv, "gazebo_client",
+            ros::init_options::NoSigintHandler);
+      }
+      this->rosNode.reset(new ros::NodeHandle());
+
+      this->srv_start = this->rosNode->advertiseService(
+          "/task_control/recepcionist/start",
+          &TaskControl_Recepcionist::OnService_Start, this);
+
+      this->pub_door_1 =
+          this->rosNode->advertise<std_msgs::String>(
+          "/sliding_door_control/door_1/command", 1000);
+
+    }
+
+    public: bool OnService_Start(
+        std_srvs::Empty::Request &_req,
+        std_srvs::Empty::Response &_res)
+    {
+      std::cout << "Start recepcionist task" << std::endl;
+      this->world->SetPaused(false);
+
+      std_msgs::String msg;
+      msg.data = "open";
+      this->pub_door_1.publish(msg);
+
+      return true;
     }
   };
 
-  // Register this plugin with the simulator
   GZ_REGISTER_WORLD_PLUGIN(TaskControl_Recepcionist)
 }
